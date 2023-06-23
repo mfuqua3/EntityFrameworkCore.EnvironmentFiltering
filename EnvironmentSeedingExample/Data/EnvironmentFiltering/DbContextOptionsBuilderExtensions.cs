b2de981ec6c00e6ab108f22192997b09443358e2
Matt Fuqua <matt.fuqua@fortyau.com>
2023-06-23 13:47:42 -0500

    xml documentation for outward facing API

--- EnvironmentSeedingExample/Data/EnvironmentFiltering/DbContextOptionsBuilderExtensions.cs
+++ EnvironmentSeedingExample/Data/EnvironmentFiltering/DbContextOptionsBuilderExtensions.cs
@@ -4,8 +4,20 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace EnvironmentSeedingExample.Data.EnvironmentFiltering;
 
+/// <summary>
+/// Provides extension methods for configuring environment seeding annotations in <see cref="DbContextOptionsBuilder"/>.
+/// </summary>
 public static class DbContextOptionsBuilderExtensions
 {
+    /// <summary>
+    /// Enables the usage of environment seeding annotations in <see cref="DbContextOptionsBuilder"/>.
+    /// </summary>
+    /// <param name="optionsBuilder">The <see cref="DbContextOptionsBuilder"/>.</param>
+    /// <returns>The <see cref="DbContextOptionsBuilder"/> with environment seeding annotations enabled.</returns>
+    /// <remarks>
+    /// This method replaces the default <see cref="IMigrationsSqlGenerator"/> and <see cref="IModelCustomizer"/> services with the environment filtering implementations.
+    /// Use this method to enable the environment filtering for data seeding operations and migration generation.
+    /// </remarks>
     public static DbContextOptionsBuilder UseEnvironmentSeedingAnnotations(this DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.ReplaceService<IMigrationsSqlGenerator, EnvironmentFilteringSqlGenerator>();
